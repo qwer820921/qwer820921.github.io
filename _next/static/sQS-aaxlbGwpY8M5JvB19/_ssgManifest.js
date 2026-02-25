b78b5af5800768d8067efdf6b8620746ac907c06
qwer820921 <89066435+qwer820921@users.noreply.github.com>
2026-02-25 09:15:53 +0000

    Deploying to gh-pages from @ qwer820921/qwer820921.github.io@e96a34b0d0a67fdda6dd3869849185e969a31a88 🚀

---
+++ _next/static/sQS-aaxlbGwpY8M5JvB19/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fblog\u002F[slug]","\u002Fnovels\u002F[id]","\u002Fnovels\u002Freader\u002F[bookId]","\u002Fnovels\u002Freader\u002F[bookId]\u002F[chapterIndex]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
